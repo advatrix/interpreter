74562b4756fbbfddbb7548677873427cdb4ecaa6
advatrix <trojan.advatrix@gmail.com>
2020-05-01 00:12:48 +0300

    nested functions

---
+++ tests/test_nested_functions.py
@@ -0,0 +1,35 @@
+import interpreter
+
+
+def test_nested_functions():
+    program = """
+    function main(argv) do
+        function inner(a) do
+            test
+        done
+    done
+    
+    """
+    intr = interpreter.Interpreter()
+    intr.interpret(program)
+    assert 'inner' in intr.sym_table[0].keys()
+
+
+def test_scope():
+    program = """
+    function outer(a) do
+        function inner(b) do
+            var c := 1
+        done
+    done
+    
+    function main(argv) do
+        int a := 1
+        inner(a)
+    done
+    
+    """
+    intr = interpreter.Interpreter()
+    intr.interpret(program)
+    assert 'inner' not in intr.sym_table[0].keys()
+
