d4ea730f64e715836a03c58016f472b6138f9054
advatrix <trojan.advatrix@gmail.com>
2020-05-01 00:30:35 +0300

    nested functions test

--- tests/test_nested_functions.py
+++ tests/test_nested_functions.py
@@ -15,7 +15,7 @@ def test_nested_functions():
     assert 'inner' in intr.sym_table[0].keys()
 
 
-def test_scope():
+def test_scope_error():
     program = """
     function outer(a) do
         function inner(b) do
@@ -33,4 +33,25 @@ def test_scope():
     intr.interpret(program)
     assert 'inner' not in intr.sym_table[0].keys()
     assert len(intr.errors) == 1
+    assert intr.errors[0].split()[0] == 'UnknownFuncError:'
 
+
+def test_scope():
+    program = """
+    function outer(a) do
+        function inner(b) do
+            b := 1
+        done
+        a := 3
+        inner(a(1))
+    done
+    
+    function main(argv) do
+        int a
+        outer(a)
+    done
+    """
+    intr = interpreter.Interpreter()
+    intr.interpret(program)
+    assert intr.sym_table[0]['a'].value[0].value == 3
+    assert intr.sym_table[0]['a'].value[1].value == 1
